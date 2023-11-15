47745576ba816710a99e8abf6dab09f3ab43e852
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-11-16 00:34:37 +0300

    everything is seen

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -11,28 +11,55 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = "__all__"
 
+
+
+
 class ProductPositionSerializer(serializers.ModelSerializer):
+
     product = ProductSerializer(read_only=True)
 
     class Meta:
         model = StockProduct
-    fields = "__all__"
+        fields = "__all__"
 
 class StockSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
     positions = ProductPositionSerializer(many=True)
+    # # positions = ProductPositionSerializer(many=True)
+    # product = ProductSerializer(many=True)
     class Meta:
         model = Stock
         fields = "__all__"
 
-    def create(self, validated_data):
-        product_data = validated_data.pop('positions')
-        stock_data = super().create(validated_data)
-        # Создание продукта
-        product = models.Product.objects.create(**product_data)
-        # stock = models.Stock.objects.create(product=product, **stock_data)
-        return stock_data
 
+    #
+    # def create(self, validated_data):
+    #     validated_data["stock"] = Stock.objects.create(**validated_data.pop("stock"))
+    #     validated_data["product"] = Product.objects.create(**validated_data.pop("product"))
+    #     StockProduct.objects.create(**validated_data)
+    #     return validated_data
+    #     def create(self, validated_data):
+    #         product_data = validated_data.pop('positions')
+    #         stock_data = (
+    #             super().create(validated_data))
+    #         # Создание продукта
+    #         product = models.Product.objects.create(**product_data)
+    #         # stock = models.Stock.objects.create(product=product, **stock_data)
+    #         return stock_data
+    # def update(self, instance, validated_data):
+    #     instance
+
+#     def create(self, validated_data):
+#         product_data = validated_data.pop('positions')
+#         stock_data = super().create(validated_data)
+#         # Создание продукта
+#         product = models.Product.objects.create(**product_data)
+#         # stock = models.Stock.objects.create(product=product, **stock_data)
+#         return stock_data
+# def create(self, validated_data):
+#     stock_product_data = validated_data.pop('stock_product')
+#     stock_product = StockProduct.objects.create(**stock_product_data)
+#     validated_data['stock_product'] = stock_product.id
+#     return Logistic.objects.create(**validated_data)
 
 # def create(self, validated_data):
 #     # достаем связанные данные для других таблиц
